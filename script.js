e23315887de74afca3b90f4a910a2b2011b7cab5
Yvonne Zertuche <94756952+YvonneOZertuche@users.noreply.github.com>
2022-10-10 17:49:45 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,59 @@
+const batteryLevel = document.querySelector(".batteryLevel")
+const batteryCharging = document.querySelector(".batteryCharging")
+const batteryChargingTime = document.querySelector(".batteryChargingTime")
+const batteryDisChargingTime = document.querySelector(".batteryDisChargingTime")
+
+//BATTERY API
+
+const battery = () => {
+  if ("getBattery" in navigator) {
+    navigator.getBattery().then(battery => {
+      function updateAllBatteryDetails() {
+        updateChrgingInfo()
+        updateLevelChange()
+        updateDischargingTimeInfo()
+        updateChargingTimeChangeInfo()
+      }
+      updateAllBatteryDetails()
+
+      //Battery Charging
+      battery.addEventListener("chargingchange", () => {
+        updateChrgingInfo()
+      })
+
+      function updateChrgingInfo() {
+        const isCharging = battery.charging ? "Yes" : "No"
+        batteryCharging.innerHTML = isCharging
+      }
+
+      //Battery Charging Time
+      battery.addEventListener("chargingtimechange", () => {
+        updateChargingTimeChangeInfo()
+      })
+
+      function updateChargingTimeChangeInfo() {
+        batteryChargingTime.innerHTML = battery.chargingTime + ' seconds'
+      }
+      //Battery Discharging time
+      battery.addEventListener("dischargingtimechange", () => {
+        updateDischargingTimeInfo()
+      })
+
+      function updateDischargingTimeInfo() {
+        batteryDisChargingTime.innerHTML = battery.dischargingTime + " seconds"
+      }
+      //Battery level change
+      battery.addEventListener("levelchange", () => {
+        updateLevelChange()
+      })
+      function updateLevelChange() {
+        const level = battery.level * 100 + "%"
+        batteryLevel.innerHTML = level
+      }
+      //Battery status
+      battery.addEventListener("batterystatus", () => {})
+    })
+  }
+}
+
+battery()
